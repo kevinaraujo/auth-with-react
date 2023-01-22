34ed57605f118d2c6018cbc87c0ce704405d238c
Kevin Araujo <kevinaraujo@Kevin-Araujos-MacBook-Pro-ArcTouch-447A.local>
2023-01-22 18:57:59 -0300

    feat: Created pedidos minha-conta routes and pedidos page. Missing add styles to it.

---
+++ src/paginas/Pedidos/index.tsx
@@ -0,0 +1,22 @@
+import { AbBotao } from "ds-alurabooks"
+
+const Pedidos = () => {
+    return (
+        <section className="pedidos">
+            <h1>Meus Pedidos</h1>
+
+           <div>
+                <ul>
+                    <li>Pedido: <strong>123</strong></li>
+                    <li>Data do pedido: <strong>09/01/2023</strong></li>
+                    <li>Valor total: <strong>R$ 42,00</strong></li>
+                    <li>Entrega realizada em: <strong>10/02/2023</strong></li>
+                </ul>
+
+                <AbBotao texto="Detalhes"/>
+           </div>
+        </section>
+    )
+}
+
+export default Pedidos
